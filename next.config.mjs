b929654c63dd0a142971d269f7b81d9ccfd814ec
AlejandroGomezE <jose.alejandro.gomez.e@hotmail.com>
2023-10-01 09:48:37 -0600

    Cors no longer needed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,3 @@
-const ContentSecurityPolicy = `
-  connect-src 'self' vitals.vercel-insights.com;
-`;
 import nextMDX from '@next/mdx';
 
 import { recmaPlugins } from './src/mdx/recma.mjs';
@@ -20,20 +17,6 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   images: {
     domains: ['images.unsplash.com'],
-  },
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
-          },
-        ],
-      },
-    ];
   },
 };
 
